018e3a0bdc7a2906a7437c0380abd68f472704ce
Mauricio Teixeira <1847440+badnetmask@users.noreply.github.com>
2023-08-09 07:42:20 -0400

    Update update-all.sh
    Fix some issues with state detection, and give better messages.

--- update-all.sh
+++ update-all.sh
@@ -5,22 +5,33 @@
 rc=0
 do_update=0
 
-for image in $(awk '/image: /{gsub("\"","");print $2}' docker-compose.yml); do
+echo "Checking for image updates using ./docker-compose.yml"
+for image in $(awk '!/#/&&/image: /{gsub("\"","");print $2}' docker-compose.yml); do
+  echo -n "${image}: "
   pull_status=$(docker pull ${image} | grep Status)
   rc=$?
   if [ $rc -ne 0 ]; then
-    echo "${image}: Docker pull appear to have failed. It's better to not continue..."
+    echo "Docker pull appear to have failed. It's better to not continue..."
     break
   fi
-  if [ "$pull_status" == *"Downloaded newer image for"* ]; then
-    echo "${image}: has been updated. Will try to restart everything."
+  if [ ! -z "echo '$pull_status' | grep 'Downloaded newer image for'" ]; then
+    echo "Image updated."
     do_update=1
+  else
+    echo "No updates available."
   fi
 done
 
 if [ $rc -eq 0 ] && [ $do_update -eq 1 ]; then
   # restart everything only if all pull succeeded, and at least one image was updated
+  echo "At least one image was updated. Attempting to restart the compose."
   docker-compose down && docker-compose rm && docker-compose up -d
+  rc=$?
+  if [ $rc -ne 0 ]; then
+    echo "Restart completed."
+  else
+    echo "Failed to restart."
+  fi
 elif [ $rc -ne 0 ]; then
   exit $rc
 else
